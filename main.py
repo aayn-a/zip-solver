53409b4208ad4a6c57007f41b5d2600891abc589
thunderistrash <99608599+aayn-a@users.noreply.github.com>
2025-11-13 18:12:56 -0800

    stuff

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+'''
+Get Puzzle
+Make it into a grid
+use an algorithm to solve
+'''
+
+
+
+import pytesseract
+import cv2
+import numpy as np
+
+image = cv2.imread("puzzle.png")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2BGRA)
+
 
